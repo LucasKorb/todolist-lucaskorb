b0148c74f0eb7dd501a5da1c2f6edff1ef97c43e
Lucas <Lucaskjoenck@gmail.com>
2021-10-05 19:00:01 -0300

    toDo Lucas Korb

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <h1>Bem vindo à minha lista de tarefas!</h1>
+      <div class="label-input">
+        <label for="input">Adicione uma tarefa no input abaixo (Enter para adicionar)</label>
+        <input type="text" v-model="novaTarefa" @keyup.enter="adicionarTarefa">
+      </div>
+    </header>
+    <RenderizedTasks :tarefas="tarefas"></RenderizedTasks>
+  </div>
+</template>
+
+<script>
+import RenderizedTasks from './components/renderizedtasks'
+
+export default {
+  name: 'App',
+  components: {RenderizedTasks},
+  data() {
+    return {
+      nome: '',
+      novaTarefa: '',
+      tarefas: [],
+    }
+  },
+  methods: {
+    adicionarTarefa() {
+      this.tarefas.push({
+        tarefa: this.novaTarefa,
+        checked: false
+      })
+      this.novaTarefa = '';
+    }
+  },
+}
+</script>
+
+<style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+.header {
+  padding: 30px;
+  background: tomato;
+}
+.label-input {
+  gap: 5px;
+  color: white;
+  display: flex;
+  flex-flow: column nowrap;
+}
+.label-input input {
+  display: inline-block;
+  padding: 3px 15px;
+  width: 600px;
+}
+h1 {
+  font-size: 3em;
+  color: white;
+}
+</style>
---
+++ src/components/renderizedtasks.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="estrutura-tasks">
+        <label v-for="task of tarefas" :key="task" class="label-task" :class=" { riscado: task.checked } ">
+            {{ task.tarefa }}
+            <input type="checkbox" v-model="task.checked">
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['tarefas'],
+}
+</script>
+
+<style>
+.estrutura-tasks {
+    padding: 30px;
+    display: flex;
+    flex-flow: column;
+    gap: 10px;
+}
+.label-task {
+    padding: 5px 30px;
+}
+.riscado {
+    color: lightgray;
+    text-decoration: line-through;
+}
+</style>
